b2de0ce0e7006fc35e359c488cb9521ab17f1d9f
Asmiath <Mr.morrow301253@yahoo.com>
2016-01-22 09:51:49 -0600

    Trivia game
    Project

---
+++ js/chQuiz.js
@@ -0,0 +1,36 @@
+/**
+	Author: Jacob Morrow
+	Created: Jan 21, 2016
+	Update: Jan 21, 2016
+	**/
+	
+	function chkQuiz(){
+		var amountCorrect = 0;
+		var amountWrong = 0;
+		var score = 0;
+		for(i=1: i < 10; i++ ){
+			
+			var choices = document.getElementByName('q' + i);
+			
+				for(j=0; j<choises.length; j++)
+					
+				{
+					var selection = choices[j];
+						if(selection.value == "correct" && selection.checked){
+							window.alert("yes")
+							amountCorrect++;
+						}else if(selection.value == "incorrect" && selection.checked){
+							amountWrong++;
+						}
+							
+				}
+			
+		}	
+		score = 5 * amountCorrect;
+		window.alert("")
+		
+		
+		
+	
+	}
+
